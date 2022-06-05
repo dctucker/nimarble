7611f62d1eabcf831d505e51b6f35448d35714ca
Casey Tucker <dctucker@hotmail.com>
2022-06-04 19:06:18 -0700

    precalculate triangle normals

--- src/leveldata.nim
+++ src/leveldata.nim
@@ -11,7 +11,7 @@ import wrapper
 import types
 import masks
 
-from models import cube_normal, cube_verts, cube_colors, cube_index, wave_res, wave_nverts, middle_points
+from models import cube_normal, cube_verts, cube_colors, cube_index, wave_res, wave_nverts, middle_points, top_points
 from scene import pos
 import assets
 
@@ -707,7 +707,9 @@ proc cube_point*(level: Level, i,j, w: int): CubePoint =
     elif vert.z == 1 and vert.x == 0: y = y2
     elif vert.z == 1 and vert.x == 1: y = y3
     elif vert.z==0.5 and vert.x==0.5:
-      if y0 == y3:
+      if (y0 == y2 and y1 == y3) or (y0 == y1 and y2 == y3):
+        y = (y0 + y3) * 0.5
+      elif y0 == y3:
         y = y0
       elif y1 == y2:
         y = (y0 + y3) * 0.5
@@ -729,20 +731,7 @@ proc cube_point*(level: Level, i,j, w: int): CubePoint =
 
   #if color_w == 4: c = vec4f(1,0,1,1)
 
-  const level_squash = 1.0
-  let na = vec3f(-1, level_squash * (y0 - y0), -1).normalize()
-  let nc = vec3f(+1, level_squash * (y1 - y0), -1).normalize()
-  let nb = vec3f(-1, level_squash * (y2 - y0), +1).normalize()
-  let nd = vec3f(+1, level_squash * (y3 - y0), +1).normalize()
-
-  surface_normal = normalize(
-    (nb - na).cross(nc - na) +
-    (nc - nb).cross(nd - nb)
-  )
-
-  if color_w == 1:
-    normal = surface_normal
-  else:
+  if color_w != 1:
     normal = cube_normal(color_w)
 
   if normal.y.classify == fcNaN:
@@ -806,7 +795,19 @@ proc calculate_color_vbo*(level: Level, i,j: int) =
       level.floor_colors[  color_offset + 2 ] = p.color.z
       level.floor_colors[  color_offset + 3 ] = p.color.w
 
-proc calculate_vbos*(level: Level, i, j, n: int, p: CubePoint) =
+proc calculate_top_normals*(level: var Level, i, j: int) =
+  const tops = top_points.sorted()
+  for s in 0..3:
+    let m = tops[s * 3]
+    var p0 = level.map[i,j].cube[m+0]
+    var p1 = level.map[i,j].cube[m+1]
+    var p2 = level.map[i,j].cube[m+2]
+    let normal = (p1.pos - p0.pos).cross(p2.pos - p1.pos).normalize()
+    p0.normal = normal
+    p1.normal = normal
+    p2.normal = normal
+
+proc calculate_vbos*(level: var Level, i, j, n: int, p: CubePoint) =
   const color_span  = 4 * cube_index.len
   const normal_span = 3 * cube_index.len
   const vert_span   = 3 * cube_index.len
@@ -821,7 +822,6 @@ proc calculate_vbos*(level: Level, i, j, n: int, p: CubePoint) =
       let v_offset = o *   vert_span + 3*m + 1
       level.floor_verts[   v_offset               ] = p.pos.y
 
-
   let color_offset = o *  color_span + 4*n
   if 0 < color_offset and color_offset < level.floor_colors.len:
     level.floor_colors[  color_offset + 0 ] = p.color.x
@@ -835,17 +835,22 @@ proc calculate_vbos*(level: Level, i, j, n: int, p: CubePoint) =
     level.floor_normals[ normal_offset + 1 ] = p.normal.y
     level.floor_normals[ normal_offset + 2 ] = p.normal.z
 
-proc calculate_vbos*(level: Level, i,j: int) =
+proc calculate_vbos*(level: var Level, i,j: int) =
   if not level.has_coord(i,j): return
 
   let o = level.index_offset(i,j) # (i-1) * floor_span + (j-7)
   if o <= 0: return
+
   for n in cube_index.low .. cube_index.high:
     let p = level.cube_point(i, j, n)
     if level.map[i,j].cube.len == 0:
       level.map[i,j].cube = newSeq[CubePoint](cube_index.len)
     level.map[i,j].cube[n] = p
 
+  level.calculate_top_normals(i,j)
+
+  for n in cube_index.low .. cube_index.high:
+    let p = level.cube_point(i, j, n)
     if p.empty: continue
     level.calculate_vbos(i,j,n, p)
 
@@ -854,7 +859,7 @@ proc reload_colors*(level: var Level) =
     level.calculate_color_vbo(i,j)
   level.update_color_vbo()
 
-proc setup_floor(level: Level) =
+proc setup_floor(level: var Level) =
   let dim = level.height * level.width
   var cx: Vec4f
   var normals = newSeqOfCap[cfloat]( dim )
@@ -894,12 +899,19 @@ proc setup_floor(level: Level) =
     for j in 1..<level.width - 1:
       if j + 4 < i or j + 4 > i + floor_span: continue
 
+      level.map[i,j].cube = newSeq[CubePoint](cube_index.len)
       for w in 0 .. cube_index.high:
-        let point = level.cube_point(i, j, w)
-        normals.add_normal point.normal
+        var point = level.cube_point(i, j, w)
 
         const margin = 0.98
         add_point point.pos.x, point.pos.y, point.pos.z, point.color
+        level.map[i,j].cube[w] = point
+
+      level.calculate_top_normals(i,j)
+
+      for w in 0 .. cube_index.high:
+        let point = level.cube_point(i, j, w)
+        normals.add_normal level.map[i,j].cube[w].normal
 
   level.floor_colors = colors
   level.floor_verts = verts
@@ -1033,7 +1045,7 @@ proc average_height*(level: Level, x,z: float): float =
   return sum / n.float
 
 
-proc apply_phase*(level: Level, i,j: int) =
+proc apply_phase*(level: var Level, i,j: int) =
   level.calculate_vbos(i,j)
 
 proc queue_update*(level: Level, update: LevelUpdate) =
@@ -1061,7 +1073,7 @@ proc process_updates*(level: var Level) =
       level.apply_update(update)
     level.updates = @[]
 
-let levels = @[
+var levels = @[
   Level(),
   init_level("0", level_data_src(0), level_mask_src(0), vec3f( 1f  , 0.0f, 1f   )),
   init_level("1", level_data_src(1), level_mask_src(1), vec3f( 1f  , 0.8f, 0f   )),
